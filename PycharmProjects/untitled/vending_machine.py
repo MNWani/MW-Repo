89b2fbb8a888afa42b09e8f163ac0ed7b789ac2d
mark <markwani70@googlemail.com>
2017-04-22 11:31:45 +0100

    Add project boilerplate

---
+++ PycharmProjects/untitled/vending_machine.py
@@ -0,0 +1,10 @@
+coins = [1, .50, .20, .10, .05, .02, .01]
+
+
+def give_change(amount):
+    change = []
+    for coin in coins:
+        while coin <= amount:
+            amount -= coin
+            change.append(coin)
+    return change
